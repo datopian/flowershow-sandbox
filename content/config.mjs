a597b1f78d99e6a3a4e7b86598cbc470c1d1be14
olayway <ola@yway.pl>
2023-05-26 16:44:31 +0200

    add basic config

--- content/config.mjs
+++ content/config.mjs
@@ -1,1 +1,16 @@
-export default {};
+const config = {
+  title: "Flowershow Sandbox",
+  description: "Flowershow Sandbox",
+  author: "Flowershow",
+  navbarTitle: {
+    text: "Flowershow",
+  },
+  editLinkRoot:
+    "https://github.com/datopian/flowershow-sandbox/edit/main",
+  showEditLink: true,
+  navLinks: [
+    { href: "/blog", name: "Blog" },
+  ],
+};
+
+export default config;
